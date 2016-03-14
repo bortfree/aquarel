1764749b30843786c71b7d8db22b75c3bb37b4ac
Evgeny Bortnikov <ebortnikov@mail.ru>
2016-03-14 21:05:25 +0300

    slider before

--- js/common.js
+++ js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
+	$(".slider").owlCarousel({
+		items: 3,
+		loop: true,
+		nav: true,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
+	});
+
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
